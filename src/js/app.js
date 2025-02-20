c9775c87a612a7e9e5424c24b55a351d1c4551a1
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2025-02-20 15:26:41 +0700

    fix code

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@
 
 import { Game } from "./Game";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const start = new Game(document.querySelector(".game-field"));
-  setInterval(() => start.randPosition(), 10000);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+const start = new Game(document.querySelector(".game-field"));
+setInterval(() => start.randPosition(), 10000);
+// });
