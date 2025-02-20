323099029dcdf1bc2be7dfb38c43cafbb7bcf69a
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2025-02-20 13:42:47 +0700

    initial

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// TODO: write code here
+
+import { Game } from "./Game";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const start = new Game(document.querySelector(".game-field"));
+  setInterval(() => start.randPosition(), 10000);
+});
